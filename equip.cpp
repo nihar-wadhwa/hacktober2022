f269c3fb154dbdc595c7ef4a2d522c7b3f031e8b
hacktober2k <114850364+hacktober2k@users.noreply.github.com>
2023-10-01 11:48:02 +0530

    Create equip.cpp

---
+++ equip.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        if (n == 1) return 1;
+    
+        int low = 0, high = n-1, leftSum = 0, rightSum = 0;
+        while (low<high) {
+            if (leftSum < rightSum) {
+                leftSum+= a[low++];
+            } else {
+                rightSum+=a[high--];
+            }
+            
+            if (leftSum == rightSum && low == high) {
+                return low+1;
+            }
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
